ee27a777ef346e82a3209ed3136d240c6134dba2
Quan Weldon <deweldo@g.clemson.edu>
2018-05-22 10:01:53 -0400

    Added the about screen.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by De'Quan Weldon on 5/22/18.
+//  Copyright © 2018 De'Quan Weldon. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
